4c53018b28dee9f1155c3af472591888d04d15c2
codebender828 <jbakebwa@163.com>
2019-01-20 22:56:42 +0800

    intial commit of vuebeijing.io website

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <img class="home__logo" src="./assets/logos/red-gold-logo.png">
+    <router-view />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style lang="scss">
+  html {
+    height: 100%;
+  }
+
+  body {
+    background: #2a2a3a;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+    color: white;
+    height: 100%;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+
+    // margin-top: 60px;
+
+    h1 {
+      font-weight: 700
+    }
+  }
+
+  .home__logo {
+    width: 300px;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Vue is coming...</h2>
+    <a href="https://vuejs.org/v2/guide/">Get Started with Vue</a>
+  </div>
+</template>
+
+<script lang="js">
+  export default {
+    name: 'Home',
+    data () {
+      return {
+        msg: 'Welcome to the VueJS Beijing Meetup!'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  @import '../scss/pages/home.scss';
+</style>

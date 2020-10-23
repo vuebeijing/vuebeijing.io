9d2ad2b4006a14834ad23684fc5b37fdce3ca9d1
codebender828 <jbakebwa@163.com>
2020-10-23 18:14:49 +0800

    chore: update icon and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,10 +7,10 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Vue Beijing official website' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
       { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' }
     ]
   },
